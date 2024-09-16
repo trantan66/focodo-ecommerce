13c7c4ad77ae3828ccb4c73f4d877ae93547c1d6
Admin <nguyenthanhtrang2733@gmail.com>
2024-09-16 23:32:08 +0700

    navigation

--- src/Components/Shared/Layout.jsx
+++ src/Components/Shared/Layout.jsx
@@ -1,12 +1,14 @@
 import { Outlet } from "react-router-dom";
 import Footer from "./Footer";
 import Header from "./Header";
+import Navigation from "./Navigation";
 
 
 export default function Layout() {
   return (
     <div>
       <Header />
+      <Navigation />
       <div className="p-40">
         <div></div>
         <div>{<Outlet />}</div>
