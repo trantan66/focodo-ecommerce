84b8de49eec7b60942f681865e23facc184fb70a
trantan66 <tranductan66vn@gmail.com>
2024-09-16 07:14:14 +0700

    admin header

--- src/Admin/Components/Shared/Layout.jsx
+++ src/Admin/Components/Shared/Layout.jsx
@@ -1,3 +1,4 @@
+import Header from "./Header";
 import Sidebar from "./Sidebar";
 import { Outlet } from "react-router-dom";
 
@@ -5,7 +6,8 @@ export default function Layout() {
   return (
     <div className="flex flex-row bg-neutral-100 h-screen w-screen overflow-hidden">
       <Sidebar />
-      <div>
+      <div className="flex-1">
+        <Header/>
         <div>{<Outlet />}</div>
       </div>
     </div>
