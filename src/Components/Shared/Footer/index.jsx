0b02c0d96ec44578d9a832f171c99f591d5453ef
Admin <nguyenthanhtrang2733@gmail.com>
2024-09-17 22:15:18 +0700

    footer

--- src/Components/Shared/Footer/index.jsx
+++ src/Components/Shared/Footer/index.jsx
@@ -1,6 +1,43 @@
-function Footer() {
+import "./Footer.css"
+import logo from "../image/logo.png"
+import React from 'react';
+import { Typography } from 'antd';
+import { TwitterOutlined, FacebookOutlined, TikTokOutlined, InstagramOutlined } from "@ant-design/icons"
+const { Title } = Typography;
+function Footer () {
   return (
-    <div className="bg-red-900">This is Footer</div>
+    <>
+      <div className="Footer">
+        <div className="container">
+          <div className="inner-wrap">
+            <div className="inner-footer-left">
+              <img src={logo} alt="logo" />
+              <p>Hệ thống quảng bá và kinh doanh đặc sản Huế</p>
+            </div>
+            <div className="inner-footer-middle">
+              <Title level={4}>Liên Hệ</Title>
+              <p>Địa chỉ: 54 Nguyễn Lương Bằng</p>
+              <p>Email: focodo-ecommerce@gmail.com</p>
+              <p>Hotline: 0123456789</p>
+            </div>
+            <div className="inner-footer-right">
+              <p>Hướng dẫn</p>
+              <p>Đặt và mua hàng</p>
+              <p>Thanh toán online</p>
+              <p>Chính sách giao hàng</p>
+              <p>Các câu hỏi thường gặp</p>
+              <p>Điều khoản sử dụng website</p>
+            </div>
+          </div>
+        </div>
+        <div className="footer-icon">
+            <TwitterOutlined />
+            <FacebookOutlined />
+            <TikTokOutlined />
+            <InstagramOutlined />
+        </div>
+      </div>
+    </>
   )
 }
 
