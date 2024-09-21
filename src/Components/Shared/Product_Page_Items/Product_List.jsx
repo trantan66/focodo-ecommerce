8d71923588c3a63d4fd6ff0a452b3e676df69849
unknown <vanminhho71@gmail.com>
2024-09-21 14:02:26 +0700

    Modify Product Page

---
+++ src/Components/Shared/Product_Page_Items/Product_List.jsx
@@ -0,0 +1,135 @@
+import { Product_Items } from "./Product_Items";
+import React, { useState } from "react";
+import { Rate } from "antd";
+import { Pagination } from "antd";
+
+const ArrangeFilter = [
+  {
+    key: "ByRating",
+    value: "Theo danh gia",
+  },
+  {
+    key: "Ascending",
+    value: "Gia tang dan",
+  },
+  {
+    key: "Descending",
+    value: "Gia giam dan",
+  },
+];
+
+function ProductList(props) {
+  return (
+    <div className="flex flex-col item-center justify-between border border-black rounded-lg w-[266.67px] h-[400px] bg-[#F6F6F6]">
+      <div className="flex justify-center items-center mt-2">
+        <Rate defaultValue={props.rate}></Rate>
+      </div>
+      <img
+        src={props.image}
+        alt=""
+        className="w-[160px] h-[160px] mx-auto mt-2 "
+      />
+      <span className="mx-auto italic font-semibold text-[16px]">
+        {props.name}
+      </span>
+      <span className="mx-auto italic font-semibold text-[24px]">
+        {props.price}
+      </span>
+      <button className="bg-black text-white mx-auto mb-3 w-[193px] h-[48px] rounded-lg hover:bg-[#3C3D37] transition duration-300">
+        Mua ngay
+      </button>
+    </div>
+  );
+}
+
+function Product_List() {
+  const [currentPage, setCurrentPage] = useState(1);
+  const [selectedCategory, setSelectedCategory] = useState("");
+  const [productsPerPage, setProductsPerPage] = useState(6);
+  const filteredData = Product_Items.filter(
+    (product) => selectedCategory === ""
+  );
+  const indexOfLastProduct = currentPage * productsPerPage;
+  const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
+
+  const currentProducts = filteredData.slice(
+    indexOfFirstProduct,
+    indexOfLastProduct
+  );
+  const handlePageChange = (page) => {
+    setCurrentPage(page);
+  };
+  const filterProducts = (products, value) => {
+    if (value === "Gia tang dan") {
+      return products.sort((a, b) => parseFloat(a.price) - parseFloat(b.price));
+    }
+
+    if (value === "Gia giam dan") {
+      return products.sort((a, b) => parseFloat(b.price) - parseFloat(a.price));
+    }
+
+    if (value === "Theo danh gia") {
+      return products.sort((a, b) => b.rate - a.rate);
+    }
+
+    return products; // Trường hợp không có filter, trả về danh sách sản phẩm gốc
+  };
+  const filteredProductsByPriceAsc = filterProducts(
+    Product_Items,
+    "Gia tang dan"
+  );
+  const filteredProductsByPriceDesc = filterProducts(
+    Product_Items,
+    "Gia giam dan"
+  );
+  const filteredProductsByRateDesc = filterProducts(
+    Product_Items,
+    "Theo danh gia"
+  );
+
+  return (
+    <div className="">
+      <div className="flex ml-10">
+        <p>
+          Số lượng sản phẩm: <span class="font-bold">85</span>
+        </p>
+        <select
+          value={selectedCategory}
+          onChange={(e) => setSelectedCategory(e.target.value)}
+          className="border border-gray-300 p-2 rounded ml-[600px]"
+        >
+          <option value="">Tat ca </option>
+          {ArrangeFilter.map((item) => (
+            <option key={item.key} value={item.value}>
+              {item.value}
+            </option>
+          ))}
+        </select>
+      </div>
+
+      <div className="grid grid-cols-3 ml-10 mt-3 gap-4 ">
+        {currentProducts.map((item, index) => (
+          <ProductList
+            key={item.id}
+            rate={item.rate}
+            image={item.image}
+            name={item.name}
+            price={item.price}
+          ></ProductList>
+        ))}
+      </div>
+      <div className="mt-5">
+        <Pagination
+          showSizeChanger={false}
+          current={currentPage}
+          onChange={handlePageChange}
+          total={Product_Items.length}
+          pageSize={productsPerPage}
+          className="flex justify-center items-center"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Product_List;
--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -1,83 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { SearchOutlined } from "@ant-design/icons";
-import { Input, Rate } from "antd";
-import banhxeonemlui from "../Components/Shared/image/banhxeo-nemlui.jpg";
-import banhbeo from "../Components/Shared/image/banhbeo.jpg";
-import banhnam from "../Components/Shared/image/banhnam.jpg";
-import comhen from "../Components/Shared/image/comhen.jpg";
-import banhloc from "../Components/Shared/image/banhloc.jpg";
-import bunbohue from "../Components/Shared/image/bunbohue.jpg";
-
-function Filter() {
-  return (
-    <div className="">
-      <label htmlFor="" className="text-xl ">
-        Danh mục sản phẩm
-      </label>
-      <div class="w-full h-0.5 bg-black mt-2 mb-4"></div>
-      <div className="mb-3">
-        <Input
-          size="large"
-          placeholder="Tìm sản phẩm"
-          prefix={<SearchOutlined />}
-        />
-      </div>
-      <div className="leading-loose">
-        <a href="" className="hover:text-orange-500 hover:no-underline">
-          Tất cả
-        </a>
-        <br></br>
-        <a href="" className="hover:text-orange-500 hover:no-underline">
-          Khuyến mãi
-        </a>
-        <br></br>
-        <a href="" className="hover:text-orange-500 hover:no-underline">
-          Bánh bèo
-        </a>
-        <br></br>
-        <a href="" className="hover:text-orange-500 hover:no-underline">
-          Bánh nậm
-        </a>
-        <br></br>
-        <a href="" className="hover:text-orange-500 hover:no-underline">
-          Cơm hến
-        </a>
-        <br></br>
-        <a href="" className="hover:text-orange-500 hover:no-underline">
-          Bánh xèo
-        </a>
-      </div>
-    </div>
-  );
-}
-
-function ProductList(props) {
-  return (
-    <div className="flex flex-col item-center justify-between border border-black rounded-lg w-[266.67px] h-[400px] bg-[#F6F6F6]">
-      <div className="flex justify-center items-center mt-2">
-        <Rate></Rate>
-      </div>
-      <img
-        src={props.image}
-        alt=""
-        className="w-[160px] h-[160px] mx-auto mt-2 "
-      />
-      <span className="mx-auto italic font-semibold text-[16px]">
-        {props.name}
-      </span>
-      <span className="mx-auto italic font-semibold text-[24px]">
-        {props.price}
-      </span>
-      <button className="bg-black text-white mx-auto mb-3 w-[193px] h-[48px] rounded-lg hover:bg-[#3C3D37] transition duration-300">
-        Mua ngay
-      </button>
-    </div>
-  );
-}
+import Filter from "../Components/Shared/Product_Page_Items/Filter";
+import Product_List from "../Components/Shared/Product_Page_Items/Product_List";
 
 function Product() {
   return (
+    
     <div>
       <div>This is Product page</div>
       <Link className="underline" to="/">
@@ -85,50 +13,7 @@ function Product() {
       </Link>
       <div className="flex justify-center">
         <Filter></Filter>
-        <div className="">
-          <div className="flex ml-10">
-            <p>
-              Số lượng sản phẩm: <span class="font-bold">85</span>
-            </p>
-            <select className="border border-gray-300 p-2 rounded ml-[600px]">
-              <option>By rating</option>
-              <option>Price low to high</option>
-              <option>Price high to low</option>
-            </select>
-          </div>
-          <div className="grid grid-cols-3 ml-10 mt-3 gap-4 ">
-            <ProductList
-              image={banhxeonemlui}
-              name="Bánh xèo"
-              price="10.000đ"
-            ></ProductList>
-            <ProductList
-              image={banhbeo}
-              name="Bánh bèo"
-              price="43.000đ"
-            ></ProductList>
-            <ProductList
-              image={banhnam}
-              name="Bánh nậm"
-              price="3.000đ"
-            ></ProductList>
-            <ProductList
-              image={comhen}
-              name="Cơm hến"
-              price="12.000đ"
-            ></ProductList>
-            <ProductList
-              image={banhloc}
-              name="Bánh lọc"
-              price="29.000đ"
-            ></ProductList>
-            <ProductList
-              image={bunbohue}
-              name="Bún bò Huế"
-              price="56.000đ"
-            ></ProductList>
-          </div>
-        </div>
+        <Product_List></Product_List>
       </div>
     </div>
   );
