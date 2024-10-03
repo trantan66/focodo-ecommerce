37d156f0f8efc3f763106b5c33f82bd595542b3b
trantan66 <tranductan66vn@gmail.com>
2024-10-03 17:14:35 +0700

    Category, review, customerdetail, another update

--- src/Admin/Components/Product/AddProduct.jsx
+++ src/Admin/Components/Product/AddProduct.jsx
@@ -1,3 +1,5 @@
+import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
+import { CKEditor } from "@ckeditor/ckeditor5-react";
 import React, { useState } from "react";
 
 const AddProduct = () => {
@@ -142,13 +144,16 @@ const AddProduct = () => {
           </div>
           <div className="mb-4">
             <span className="block text-white mb-2">Mô tả chi tiết</span>
-            <textarea
-              value={description}
-              onChange={(e) => setDescription(e.target.value)}
-              className="w-full p-3 border rounded-sm bg-[#282941] text-white focus:outline-none"
-              placeholder="Nhập mô tả sản phẩm"
-              rows="4"
-              required
+            <CKEditor
+              editor={ClassicEditor}
+              data={description}
+              onChange={(event, editor) => {
+                const data = editor.getData();
+                setDescription(data);
+              }}
+              config={{
+                placeholder: "Nhập mô tả chi tiết sản phẩm...",
+              }}
             />
           </div>
 
