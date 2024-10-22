e2ecc0e6be5e30808e419d552173dd973d3e4161
Admin <nguyenthanhtrang2733@gmail.com>
2024-10-22 19:42:59 +0700

    cart_header

---
+++ src/Components/Shared/Header/dataCart.js
@@ -0,0 +1,32 @@
+const productsCart = [
+    {
+        id: 1,
+        Name: 'Bánh bèo Huế',
+        OriginalPrice: 20000,
+        Quantity: 1,
+        image: 'https://cdn.tgdd.vn/2021/07/CookProduct/bthum-1200x676-2.jpg',
+    },
+    {
+        id: 2,
+        Name: 'Bánh bèo Huế',
+        OriginalPrice: 20000,
+        Quantity: 1,
+        image: 'https://cdn.tgdd.vn/2021/07/CookProduct/bthum-1200x676-2.jpg',
+    },
+    {
+        id: 3,
+        Name: 'Bánh bèo Huế',
+        OriginalPrice: 20000,
+        Quantity: 1,
+        image: 'https://cdn.tgdd.vn/2021/07/CookProduct/bthum-1200x676-2.jpg',
+    },
+    {
+        id: 4,
+        Name: 'Bánh bèo Huế',
+        OriginalPrice: 20000,
+        Quantity: 1,
+        image: 'https://cdn.tgdd.vn/2021/07/CookProduct/bthum-1200x676-2.jpg',
+    },
+];
+
+export default productsCart;
--- src/Components/Shared/Header/index.jsx
+++ src/Components/Shared/Header/index.jsx
@@ -4,6 +4,7 @@ import { Input } from 'antd';
 import './Header.css'; // Giữ lại import CSS của bạn
 import logo from '../image/logo.png';
 import products from './data';
+import productsCart from './dataCart';
 
 function Header() {
     const [isSearchActive, setIsSearchActive] = useState(false);
@@ -75,27 +76,55 @@ function Header() {
                         <div className="inner-icon">
                             <ShoppingCartOutlined onClick={toggleCart} />
                             {isCartVisible && (
-                                <div className="cart-overlay" onClick={handleOverlayClick}>
-                                    <div className="cart">
-                                        <div className="products">
-                                            {visibleProducts.map((product) => (
-                                                <div key={product.id} className="product-item flex items-center mb-2">
+                                <div
+                                    className="cart-overlay fixed top-0 left-0 w-screen h-screen bg-black bg-opacity-50 z-10"
+                                    onClick={handleOverlayClick}
+                                >
+                                    <div className="w-[450px] h-[550px] bg-white mt-[60px] ml-[1000px] p-[20px] rounded-xl shadow-md">
+                                        <p className="font-medium text-center text-xl mb-[10px]">GIỎ HÀNG</p>
+                                        <div className="max-h-[300px] overflow-y-auto scrollbar">
+                                            {productsCart.map((product) => (
+                                                <div key={product.id} className="flex items-center mb-2">
                                                     <img
                                                         src={product.image}
                                                         alt={product.Name}
-                                                        className="product-image"
+                                                        className="w-[75px] h-[75px] object-cover"
                                                     />
-                                                    <div className="product-details ml-2">
-                                                        <span className="product-name">{product.Name}</span>
-                                                        <span className="product-quantity">{product.Quantity}</span>
-                                                        <span className="product-price">
-                                                            {product.OriginalPrice} VND
-                                                        </span>
+                                                    <div className="ml-[10px] flex justify-between w-full">
+                                                        <div>
+                                                            <p className="font-medium text-lg">{product.Name}</p>
+                                                            <p>x{product.Quantity}</p>
+                                                        </div>
+                                                        <div className="mr-[10px] text-red-500">
+                                                            {formatCurrency(product.OriginalPrice)}
+                                                        </div>
                                                     </div>
                                                 </div>
                                             ))}
                                         </div>
-                                        <div className="price">Tổng tiền: {/* Tính tổng giá ở đây */}</div>
+
+                                        <div className="mt-[15px] mb-[15px] space-y-1">
+                                            <div className="w-full flex justify-between font-medium text-lg ">
+                                                <p>Tạm tính:</p>
+                                                <p className="">{formatCurrency(150000)}</p>
+                                            </div>
+                                            <div className="w-full flex justify-between font-medium text-lg ">
+                                                <p>Giảm giá:</p>
+                                                <p className="">{formatCurrency(20000)}</p>
+                                            </div>
+                                            <div className="w-full flex justify-between font-medium text-lg ">
+                                                <p>Tổng tiền:</p>
+                                                <p className="text-red-500">{formatCurrency(130000)}</p>
+                                            </div>
+                                        </div>
+                                        <div className="w-full flex justify-between">
+                                            <button className="w-[48%] h-[50px] font-medium rounded border border-black-500">
+                                                Chỉnh sửa giỏ hàng
+                                            </button>
+                                            <button className="w-[48%] h-[50px] font-medium rounded border border-black bg-black text-white">
+                                                Thanh toán
+                                            </button>
+                                        </div>
                                     </div>
                                 </div>
                             )}
