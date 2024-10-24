cf4131e87f502351e4522fee3057e95286918e8d
Hung22072003 <tranngocnhathung2207@gmail.com>
2024-10-24 14:19:51 +0700

    refactor axios

--- src/Services/Customize-Axios.js
+++ src/Services/Customize-Axios.js
@@ -1,38 +1,38 @@
-import axios from "axios";
+import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:8080/api/v1/",
+    baseURL: 'http://localhost:8080/api/v1/',
 });
 
-// axiosInstance.interceptors.request.use(
-//   (config) => {
-//     const token = localStorage.getItem("access_token");
-//     if (token) {
-//       config.headers["Authorization"] = `Bearer ${token}`;
-//     }
+axiosInstance.interceptors.request.use(
+    (config) => {
+        const access_token = localStorage.getItem('access_token');
+        if (access_token) {
+            config.headers['Authorization'] = `Bearer ${access_token}`;
+        }
 
-//     return config;
-//   },
-//   (error) => {
-//     return Promise.reject(error);
-//   }
-// );
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    },
+);
 
 axiosInstance.interceptors.response.use(
-  (response) => {
-    return response.data;
-  },
-  (error) => {
-    if (error.response) {
-      console.error("Error status:", error.response.status);
-      console.error("Error data:", error.response.data);
-    } else if (error.request) {
-      console.error("No response received:", error.request);
-    } else {
-      console.error("Error setting up request:", error.message);
-    }
-    return Promise.reject(error);
-  }
+    (response) => {
+        return response.data;
+    },
+    (error) => {
+        if (error.response) {
+            console.error('Error status:', error.response.status);
+            console.error('Error data:', error.response.data);
+        } else if (error.request) {
+            console.error('No response received:', error.request);
+        } else {
+            console.error('Error setting up request:', error.message);
+        }
+        return Promise.reject(error);
+    },
 );
 
 export default axiosInstance;
